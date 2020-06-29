b4c0c57bf3a803ce27ddc34ec35e2e9e0b3c9e50
، Kahrbaa <37830834+iiTzK@users.noreply.github.com>
2020-06-29 20:23:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://kahrbaa-mu.glitch.me/`);
-}, 280000);
- 
-
 const Discord = require('discord.js');
 const converter = require('number-to-words');
 const moment = require('moment');
@@ -76,6 +64,24 @@ client.on('ready', () => {
     console.log(`Guilds: ${client.guilds.size}`);
     console.log(`Users: ${client.users.size}`);
     client.user.setActivity(`’Music{KAHRBA} | ${prefix}help`,{type: 'Playing'});
+});
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
 });
 
 client.on('message', async msg => {
